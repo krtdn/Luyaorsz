6649f9313cade6ea970272c7cda5ef2824f15427
krtdnl_r <kurtdanielrentino@gmail.com>
2025-02-16 22:08:46 +0800

    Add files via upload

---
+++ NumberGuessingApp/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace NumberGuessingApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            play_button = new Button();
+            textBox1 = new TextBox();
+            exit_button = new Button();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // play_button
+            // 
+            play_button.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            play_button.BackColor = Color.Bisque;
+            play_button.BackgroundImageLayout = ImageLayout.None;
+            play_button.FlatStyle = FlatStyle.Popup;
+            play_button.Font = new Font("Cooper Black", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            play_button.ForeColor = SystemColors.ControlDarkDark;
+            play_button.Location = new Point(123, 244);
+            play_button.Name = "play_button";
+            play_button.Size = new Size(89, 43);
+            play_button.TabIndex = 0;
+            play_button.Text = "Guess";
+            play_button.UseVisualStyleBackColor = false;
+            play_button.Click += play_button_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Arial Rounded MT Bold", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(181, 183);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(113, 32);
+            textBox1.TabIndex = 1;
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // exit_button
+            // 
+            exit_button.BackColor = Color.Silver;
+            exit_button.FlatStyle = FlatStyle.Popup;
+            exit_button.Font = new Font("Cooper Black", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            exit_button.ForeColor = Color.DimGray;
+            exit_button.Location = new Point(261, 244);
+            exit_button.Name = "exit_button";
+            exit_button.Size = new Size(90, 43);
+            exit_button.TabIndex = 3;
+            exit_button.Text = "Exit";
+            exit_button.UseVisualStyleBackColor = false;
+            exit_button.Click += exit_button_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(-5, -154);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(523, 282);
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.LavenderBlush;
+            button1.BackgroundImageLayout = ImageLayout.None;
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.Font = new Font("Cooper Black", 11.25F);
+            button1.ForeColor = SystemColors.ControlDarkDark;
+            button1.Location = new Point(191, 314);
+            button1.Name = "button1";
+            button1.Size = new Size(89, 34);
+            button1.TabIndex = 5;
+            button1.Text = "HINT!!!";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LemonChiffon;
+            BackgroundImageLayout = ImageLayout.None;
+            ClientSize = new Size(471, 403);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Controls.Add(exit_button);
+            Controls.Add(textBox1);
+            Controls.Add(play_button);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Location = new Point(20, 20);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            Text = "Number Guessing";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button play_button;
+        private Button hint_button;
+        private TextBox textBox1;
+        private Button exit_button;
+        public PictureBox pictureBox1;
+        private Button button1;
+    }
+}
---
+++ NumberGuessingApp/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace NumberGuessingApp
+{
+    public partial class Form1 : Form
+    {
+        private int guessNumber;  
+        private int guessTries = 3;  
+        private bool hintUsed = false;  
+        private Random random = new Random(); 
+
+        public  Form1()
+        {
+            InitializeComponent();
+            StartNewGame();
+        }
+
+     
+        private void StartNewGame()
+        {
+            guessNumber = random.Next(1, 10); 
+            guessTries = 3;  
+            hintUsed = false; 
+            MessageBox.Show("New game started! You have 3 tries and 1 hint.");
+        }
+
+        // Event handler for the Guess button (play_button)
+        private void play_button_Click(object sender, EventArgs e)
+        {
+            if (guessTries <= 0)
+            {
+                MessageBox.Show("Game Over! No more guesses left.\nStarting a new game...");
+                StartNewGame();
+                return;
+            }
+
+            Match matchNumber = Regex.Match(textBox1.Text, @"^\d+$"); // Check if input is a number
+            if (!matchNumber.Success)
+            {
+                MessageBox.Show("Please enter a valid number!", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int userGuess = Convert.ToInt32(textBox1.Text); // Convert input to integer
+
+            if (userGuess == guessNumber)
+            {
+                MessageBox.Show($"You guessed it right!\nThe correct number was {guessNumber}");
+                StartNewGame(); 
+            }
+            else
+            {
+                guessTries--; // Reduces yung tries
+                if (guessTries > 0)
+                {
+                    MessageBox.Show($"❌ Wrong! Try again.\nYou have {guessTries} guesses left.");
+                }
+                else
+                {
+                    MessageBox.Show($"❌ Game Over! The correct number was {guessNumber}.\nStarting a new game...");
+                    StartNewGame();
+                }
+            }
+
+            textBox1.Clear();
+        }
+
+   
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (hintUsed)
+            {
+                MessageBox.Show("You have already used your hint!");
+                return;
+            }
+
+            Match matchNumber = Regex.Match(textBox1.Text, @"^\d+$");
+            if (!matchNumber.Success)
+            {
+                MessageBox.Show("Enter a number first to get a hint!", "Hint Needed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            int userGuess = Convert.ToInt32(textBox1.Text);
+            int difference = Math.Abs(guessNumber - userGuess); 
+
+            string hint;
+            if (difference == 0)
+                hint = "You already guessed the correct number!";
+            else if (difference <= 2)
+                hint = "Very close!";
+            else if (difference <= 4)
+                hint = "Close!";
+            else
+                hint = "Far away.,.,";
+
+            MessageBox.Show($" Hint: {hint}");
+            hintUsed = true; 
+        }
+
+        private void exit_button_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Do you want to exit?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dr == DialogResult.Yes)
+            {
+                MessageBox.Show("Thank you for playing! uWu");
+                this.Close();
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
